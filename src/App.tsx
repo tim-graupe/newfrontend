0d184c8834e572c4f9eadeea3a2aad84f30a9016
tim-graupe <tgraupe90@gmail.com>
2024-01-28 15:26:03 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Login from './auth/Login';
 function App() {
+  const handleLogin = () => {
+    // Redirect the user to the server-side login route
+    window.location.href = 'http://localhost:4000/auth/google';
+  };
+
+
+  useEffect(() => {
+
+    fetch("http://localhost:4000/", {
+      credentials: 'include'
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login />
     </div>
   );
 }
