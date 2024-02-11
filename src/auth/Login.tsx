e5dbaff89f38e72963c83541916351a87c971a58
tim-graupe <tgraupe90@gmail.com>
2024-02-11 10:40:38 -0500

    change form type

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -125,7 +125,8 @@ export default function Login(): JSX.Element {
 
                     {/* <!-- Right column container with form --> */}
                     <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
-                        <form>
+                        <form onSubmit={handleLogin}>
+
                             {/* <!-- Email input --> */}
                             <TEInput
                                 onChange={(e) => setEmail(e.target.value)}
@@ -176,7 +177,7 @@ export default function Login(): JSX.Element {
                             <TERipple rippleColor="light" className="w-full">
                                 <button
                                     onClick={handleLogin}
-                                    type="button"
+                                    type="submit"
                                     className="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                                 >
                                     Sign in
@@ -184,7 +185,7 @@ export default function Login(): JSX.Element {
                             </TERipple>
 
                             <button
-                                type="button"
+                                type="submit"
                                 className="mb-2 block w-full rounded border-2 border-primary px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                                 onClick={handleGuestLogin}
                             >
