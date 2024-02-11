35f1c193cb2ed611cfc6a1cf685bb573eb56e157
tim-graupe <tgraupe90@gmail.com>
2024-02-11 10:46:13 -0500

    undo

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { TERipple } from "tw-elements-react";
-import GoogleButton from 'react-google-button';
+import { Link } from "react-router-dom";
 import config from "../config";
-
+import { TEInput, TERipple } from "tw-elements-react";
+import GoogleButton from 'react-google-button'
 export default function Login(): JSX.Element {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -16,10 +16,50 @@ export default function Login(): JSX.Element {
             ? config.development.apiUrl
             : config.production.apiUrl;
 
+
+
     const handleGoogleLogin = () => {
         // Redirect the user to the server-side login route
-        window.location.href = `${apiUrl}/auth/google`;
+        window.location.href = `/auth/google`;
     };
+
+    const test = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        fetch(`https://newbackend-production-6c67.up.railway.app`, {
+            credentials: 'include',
+            mode: 'cors'
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        fetch(`${apiUrl}/`, {
+            credentials: 'include',
+            mode: 'cors'
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        fetch(`${apiUrl}`, {
+            credentials: 'include',
+            mode: 'cors'
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
 
     const handleLogin = (e: React.FormEvent) => {
         e.preventDefault();
@@ -70,9 +110,11 @@ export default function Login(): JSX.Element {
     };
 
     return (
+
         <section className="h-screen">
             <div className="container h-full px-6 py-24">
                 <div className="flex flex-wrap items-center justify-center h-full g-6 lg:justify-between">
+                    {/* <!-- Left column container with background--> */}
                     <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
                         <img
                             src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
@@ -80,21 +122,87 @@ export default function Login(): JSX.Element {
                             alt="Phone"
                         />
                     </div>
+
+                    {/* <!-- Right column container with form --> */}
                     <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
                         <form onSubmit={handleLogin}>
-                            <div>
-                                {/* ... Email input and Password input components */}
+
+                            {/* <!-- Email input --> */}
+                            <TEInput
+                                onChange={(e) => setEmail(e.target.value)}
+                                type="email"
+                                label="Email address"
+                                size="lg"
+                                className="mb-6"
+                            ></TEInput>
+
+                            {/* <!--Password input--> */}
+                            <TEInput
+                                onChange={(e) => setPassword(e.target.value)}
+                                type="password"
+                                label="Password"
+                                className="mb-6"
+                                size="lg"
+                            ></TEInput>
+                            <button onClick={test}>Test</button>
+                            {/* <!-- Remember me checkbox --> */}
+                            <div className="flex items-center justify-between mb-6">
+                                <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
+                                    <input
+                                        className="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                                        type="checkbox"
+                                        value=""
+                                        id="exampleCheck3"
+                                        defaultChecked
+                                    />
+                                    <label
+                                        className="inline-block pl-[0.15rem] hover:cursor-pointer"
+                                        htmlFor="exampleCheck3"
+                                    >
+                                        Remember me (not active)
+                                    </label>
+                                </div>
+
+                                {/* <!-- Forgot password link --> */}
+                                {/* <a
+                                    href="#!"
+                                    className="transition duration-150 ease-in-out text-primary hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+                                >
+                                    Forgot password?
+                                </a> */}
                             </div>
+
+                            {/* <!-- Submit button --> */}
+
                             <TERipple rippleColor="light" className="w-full">
                                 <button
+                                    onClick={handleLogin}
                                     type="submit"
                                     className="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                                 >
                                     Sign in
                                 </button>
                             </TERipple>
-                            {/* ... Guest login button */}
-                            <GoogleButton onClick={handleGoogleLogin} />
+
+                            <button
+                                type="submit"
+                                className="mb-2 block w-full rounded border-2 border-primary px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                                onClick={handleGuestLogin}
+                            >
+                                Just visiting? Click here for a guest account
+                            </button>
+                            {/* <!-- Divider --> */}
+                            <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+
+                                <p className="mx-4 mb-0 font-semibold text-center dark:text-neutral-200">
+                                    OR
+                                </p>
+                            </div>
+                            <GoogleButton
+                                onClick={handleGoogleLogin}
+                            />
+
+
                         </form>
                     </div>
                 </div>
