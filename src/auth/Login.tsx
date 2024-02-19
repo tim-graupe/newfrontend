64a7533f8721556a05769c57fbd3290870827d09
tim-graupe <tgraupe90@gmail.com>
2024-02-19 13:20:44 -0500

    login redirects - possible working build

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -17,6 +17,7 @@ export default function Login(): JSX.Element {
             : config.production.apiUrl;
 
 
+
     //login functions
 
     const handleGoogleLogin = () => {
@@ -42,6 +43,9 @@ export default function Login(): JSX.Element {
                 console.log(data);
                 if (data.error) {
                     console.log(data);
+                } else {
+                    window.location.href = "/";
+
                 }
             })
             .catch(err => {
@@ -66,12 +70,17 @@ export default function Login(): JSX.Element {
                 console.log(data);
                 if (data.error) {
                     console.log(data);
+                } else {
+                    window.location.href = "/";
                 }
             })
             .catch(err => {
                 console.log(err);
             });
     };
+
+
+
 
     return (
         <section className="h-screen">
@@ -114,15 +123,15 @@ export default function Login(): JSX.Element {
                                 <button
                                     onClick={handleLogin}
                                     type="submit"
+                                    className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
 
                                 >
                                     Sign in
                                 </button>
                             </TERipple>
-
                             <button
                                 type="submit"
-
+                                className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
                                 onClick={handleGuestLogin}
                             >
                                 Just visiting? Click here for a guest account
@@ -138,8 +147,8 @@ export default function Login(): JSX.Element {
                                 onClick={handleGoogleLogin}
                             />
 
-
                         </form>
+                        <p>New User? <a href="/register" className="transition duration-150 ease-in-out text-danger hover:text-danger-600 focus:text-danger-600 active:text-danger-700">Register</a></p>
                     </div>
                 </div>
             </div>
